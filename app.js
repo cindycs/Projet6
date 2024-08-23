3bee70cfd478985ed3864912bf20785afdccb786
cindycs <schaal.cindy@gmail.com>
2024-08-23 10:13:21 +0200

    connexion avec la base de donnée

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+//const booksRoutes = require('.routes/book');
 
 const app = express();
 
+
+mongoose.connect('mongodb+srv://cindycs:uLIspO4itC0z9Tdl@cluster0.9tgri.mongodb.net/')
+
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
 app.use((req, res, next) => {
-    res.json( { message: 'requete reçu '})
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 })
+
+
+/*
+app.use('/api/books', booksRoutes );
+*/
 
 module.exports = app;
